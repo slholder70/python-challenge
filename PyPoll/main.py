5a47c9b18606f5a95f5bfa777c6009fdba9d12af
SixFour <slholder70@gmail.com>
2018-08-16 11:36:47 -0400

    PyPoll progress

---
+++ PyPoll/main.py
@@ -0,0 +1,30 @@
+import csv
+
+# store file path associated with the file
+
+file1 = 'raw_data/election_data_1.csv'
+
+file2 = 'raw_data/election_data_2.csv'
+
+# open the file in "read" mode ('r') and store contents in variables edata1, #  edata2
+
+#with open(file1, 'r') as text:
+ #   print (text)
+    
+#    edata1 = text.read()  #stored file text as edata1
+    
+   # print(edata1)
+    
+#print(type(edata1))     #edata1 is a string and not a dataframe
+
+#Method 2 for improved reading of CSV module
+
+with open(file1, newline ='') as edata1:
+    
+    #CSV reader with delimiter and varable holding contents
+    
+    csvreader = csv.reader(edata1, delimiter = ',')
+    print(csvreader)
+    
+    for row in csvreader:
+        print(row)
