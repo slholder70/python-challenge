1a5d6fbdfca6c38933275916b78b9fdc5720ebce
SixFour <slholder70@gmail.com>
2018-08-21 17:42:20 -0400

    no concat add a secod reader for edata2

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,22 @@
 import csv
 
+#Assignment of variables for calculations        
+
+vote_ct = 0             #The total number of votes cast
+listofcand = []         #A complete list of candidates who received votes
+vote_percentlist = []   #The list of percentage of votes each candidate won
+votes_per_candlist = [] #The list of total number of votes each candidate won
+election_winner = []    #The winner of the election based on popular vote.
+
+
+
 # store file path associated with the file
 
 file1 = 'raw_data/election_data_1.csv'
 
 file2 = 'raw_data/election_data_2.csv'
 
-# open the file in "read" mode ('r') and store contents in variables edata1, #  edata2
+# open the file in "read" mode ('r') and store contents in variables edata1,edata2
 
 #with open(file1, 'r') as text:
 #    print (text)
@@ -19,7 +29,7 @@ file2 = 'raw_data/election_data_2.csv'
 
 #Method 2 for improved reading of CSV module
 
-with open(file2, newline ='') as edata:
+with open(file1, newline ='') as edata:
     
     #CSV reader with delimiter and varable holding contents
     
@@ -27,20 +37,12 @@ with open(file2, newline ='') as edata:
 #   print(csvreader)
     
     for row in edata1:
-        mylist1 = row[0]
-        
-print(mylist1)
-#Assignment of variables for calculations        
-#The total number of votes cast
-vote_ct = 0
-#A complete list of candidates who received votes
-listofcand = []
-#The list of percentage of votes each candidate won
-vote_percentlist = []
-#The list of total number of votes each candidate won
-votes_per_candlist = []
-#The winner of the election based on popular vote.
-election_winner = []
+        if row[2] in listofcand:
+            print("already here")# add vote to candidate in listofcand
+        else:
+            listofcand.append(row[2])   
+
+
         
          
 print("Election Results")            
