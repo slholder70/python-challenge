5662c8d20e01944b5662b34be7927f7f2e72d832
SixFour <slholder70@gmail.com>
2018-08-22 00:24:35 -0400

    using spyder to debug. successfully ran iteration for edata1

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,7 +9,6 @@ votes_per_candlist = [] #The list of total number of votes each candidate won
 election_winner = []    #The winner of the election based on popular vote.
 
 
-
 # store file path associated with the file
 
 file1 = 'raw_data/election_data_1.csv'
@@ -34,14 +33,24 @@ with open(file1, newline ='') as edata:
     #CSV reader with delimiter and varable holding contents
     
     edata1 = csv.reader(edata, delimiter = ',')
-#   print(csvreader)
-    
+    next(edata1)    #skip header 
     for row in edata1:
-        if row[2] in listofcand:
-            print("already here")# add vote to candidate in listofcand
+        vote_ct += 1
+        
+        if row[2] not in listofcand:
+            listofcand.append(row[2])   #adding new name to listofcand
+            votes_per_candlist.append(1)  #adding one vote to the new name added to votes list
+            
         else:
-            listofcand.append(row[2])   
+            #list_index =  #index to pair listofcand and votes_per_candlist
+            #adding vote to candidate in list by indexing the lists
+            votes_per_candlist[int(listofcand.index(row[2]))] = int(votes_per_candlist[int(listofcand.index(row[2]))]) + 1 
+            
 
+#with open(file2,  newline ='') as edata:
+    
+#    edata2 = csv.reader(edata, delimeter = ',')
+#    next(edata2)
 
         
          
